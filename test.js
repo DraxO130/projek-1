299076e2d60595c893a73cb9bdacb7703efd9046
Adit <fransiscowidiyanto@gmail.com>
2025-01-08 02:15:31 +0700

    projek_1

---
+++ test.js
@@ -0,0 +1,23 @@
+let a,b,c,d,e,f;
+a = 10;
+b = 10;
+c = a + b;
+d = " namaku";
+e = " Fransisco";
+f = " Aditya";
+g = d + e + f;
+
+function ajg() {
+    document.getElementById("coba").innerHTML = "Paragraph changed.";
+  }
+
+  document.write("hallo " + g);
+  document.write(", umurku " + c + " tahun" );
+    alert("selamat datang");
+
+    
+
+function tampil(){
+    document.getElementById("tampil1").innerHTML = "halo semuanya";
+    document.getElementById("tampil2").innerHTML = "Senang bisa belajar";
+}
